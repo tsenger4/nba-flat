fc66024f03886b6f528aaf23e9bfe646fc65bc9f
Tobias Senger <tsenger4@Tobiass-MacBook-Air.local>
2022-11-15 16:40:12 -0300

    Added search for current NBA players stats

---
+++ players-parser.js
@@ -0,0 +1,33 @@
+import { readJSON, writeJSON } from 'https://deno.land/x/flat/mod.ts'
+
+const filename = Deno.args[0]
+const data = await readJSON(filename)
+
+const newfile = `${filename.replace(".json", "")}_parsed.json`
+const data_minimal = data.league.standard
+
+await writeJSON(newfile, data_minimal);
+
+const year = new Date().getFullYear() - 1;
+
+for (var i in data_minimal) {
+
+    let el = data_minimal[i]
+
+    try {
+        let fullName = `${el.firstName} ${el.lastName}`;
+        let response = await fetch("https://www.balldontlie.io/api/v1/players?search=" + fullName);
+
+        let obj = JSON.parse(await response.text());
+        let id = obj.data[0].id;
+
+        let response1 = await fetch("https://www.balldontlie.io/api/v1/stats?seasons[]=" + year + "&player_ids[]=" + id);
+        let obj1 = JSON.parse(await response1.text());
+
+        await writeJSON(`./players/${el.teamSitesOnly.playerCode}.json`, obj1.data);
+        await new Promise(r => setTimeout(r, 2000));
+    }
+    catch (err) {
+        console.log("fetch failed", err);
+    }
+};
